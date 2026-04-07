a5f2200b2d88f7ecb2c021f4f722e621da3c4170
Brandon <brandon@Brandons-MacBook-Pro.local>
2026-04-07 14:33:20 -0700

    add react stuff

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App: React.FC = () => {
     return (
-        <section className=''>
+        <section className='container'>
             <h1>Recipe browser</h1>            
         </section>
     )
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 import {viteStaticCopy} from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
