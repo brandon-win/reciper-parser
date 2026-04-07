d64d722474daa40a911a4db3d0102a2d9e283494
Brandon <brandon@Brandons-MacBook-Pro.local>
2026-04-07 14:31:40 -0700

    first iteration of chrome extension

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,26 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import {viteStaticCopy} from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/manifest.json',
+          dest: '.'
+        }
+      ]
+    })
   ],
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      }
+    }
+  }
 })
